dc5a4b8d4bcb3f60c8fb9d49d01ed2f20c7f2163
unknown <asim26k@gmail.com>
2024-10-27 13:01:12 +0530

    Improved Websocket Server

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,24 @@ import { WebSocketServer } from "ws";
 const wss = new WebSocketServer({ port: 8080 });
 
 wss.on("connection", (ws) => {
-  console.log("Someone joined");
+  console.log("New Client connected");
   ws.on("error", () => {
     console.log("Error");
   });
   ws.on("message", (data) => {
-    console.log("Message:", data.toString());
-    ws.send(`Your message: ${data}`);
+    console.log("Recieved:", data.toString());
+    // Broadcast the messagge to all clients
+    wss.clients.forEach((client) => {
+      // Currently broadcasting back to the sender itself too.
+      if (client.readyState === client.OPEN) {
+        client.send(JSON.stringify({ message: data.toString() }));
+      }
+    });
   });
+
+  ws.on("close", () => {
+    console.log("Client dissconnected");
+  });
+
   ws.send("Something");
 });
